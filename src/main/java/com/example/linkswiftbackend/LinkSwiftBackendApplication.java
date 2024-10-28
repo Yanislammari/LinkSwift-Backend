344838504839ac65f21718416651dbb12ab2867c
yanislammari <yanis.lammari937@gmail.com>
2024-10-28 13:03:53 +0100

    initial commit

---
+++ src/main/java/com/example/linkswiftbackend/LinkSwiftBackendApplication.java
@@ -0,0 +1,11 @@
+package com.example.linkswiftbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinkSwiftBackendApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(LinkSwiftBackendApplication.class, args);
+    }
+}
