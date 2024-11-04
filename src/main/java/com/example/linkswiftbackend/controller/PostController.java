d38c0c39bdce9d826c18586e1c47e401abfa3a85
yanislammari <yanis.lammari937@gmail.com>
2024-11-05 00:43:55 +0100

    add post post request

--- src/main/java/com/example/linkswiftbackend/controller/PostController.java
+++ src/main/java/com/example/linkswiftbackend/controller/PostController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 import java.util.List;
 import java.util.UUID;
 
@@ -34,6 +35,23 @@ public class PostController {
         }
         catch(Exception e) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body("{\"message\": \"Post not found\"}");
+        }
+    }
+
+    @PostMapping("/addPost/{userId}")
+    public ResponseEntity<?> addPost(@RequestParam("content") String content, @RequestParam("categories") List<String> categories, @RequestParam(value = "media", required = false) MultipartFile media, @PathVariable("userId") UUID userId) {
+        try {
+            byte[] mediaBytes = null;
+            if(media != null && !media.isEmpty()) {
+                mediaBytes = media.getBytes();
+            }
+            PostDto post = new PostDto(null, content, categories, mediaBytes, userId);
+            PostDto addedPost = postService.addPost(post, userId);
+            return ResponseEntity.ok(addedPost);
+        }
+        catch(Exception e) {
+            System.out.println(e.getMessage());
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("{\"message\": \"Failed to add post.\"}");
         }
     }
 }
